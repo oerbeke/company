d1c596dcb881618fc02a6b1e43436b8b8188bf4f
29536896 <aaronsoennichsen@gmail.com>
2020-11-05 15:28:59 +0100

    port update

--- company/config.js
+++ company/config.js
@@ -1,7 +1,7 @@
 // config.js
 const config = {
     databaseURI: 'mongodb+srv://Aaron:Anarchie99@cluster0.dfoma.mongodb.net/company?retryWrites=true&w=majority', // or MongoDB Atlas connection URI
-    localPort: 0
+    localPort: 8080
 };
 
 module.exports = config;
